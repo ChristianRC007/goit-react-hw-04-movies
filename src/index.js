2117b87f81b93515a7b9df6ff2fe6f83791782cc
Andrey Cherniak <baza904@gmail.com>
2021-06-03 02:03:48 +0300

    reset to old code version

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import './styles/base.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
